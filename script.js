c4f0905e8cd2a44b122ef76f3c5646ca33769647
Doug Enas <douglassenas@gmail.com>
2018-08-23 09:27:53 -0400

    more changes to game board

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ let clickNum = 1
 var currentPlayer = "";
 
 const gameColumns = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
 
 ]
 
@@ -27,11 +27,7 @@ for (let i = 0; i < gameColumns.length; i++) {
     gameboard.appendChild(column)
 }
 
-function newId(p) {
-    let removeDash = p.split("-");
-    let removeLetters = removeDash.pop();
-    return removeLetters;
-}
+
 
 
 
@@ -39,7 +35,6 @@ const playChip = function(event) {
 
     let currentCol = event.currentTarget;
 
-    let idPiece = newId(currentCol.id)
 
     let disc = document.createElement("div");
 
@@ -58,7 +53,6 @@ const playChip = function(event) {
 
 
         disc.className = ('red');
-        gameColumns[idPiece][x + y] = 1
         currentPlayer = "Red";
 
 
@@ -69,7 +63,6 @@ const playChip = function(event) {
 
     } else {
         disc.className = ('yellow');
-        gameColumns[idPiece][x + y] = 2
         currentPlayer = "Yellow";
 
 
@@ -84,77 +77,6 @@ const playChip = function(event) {
 }
 
 
-const winVertical = function() {
-
-    for (let y = 0; y < gameColumns.length; y++) {
-        let row = gameColumns[y]
-
-        for (let x = 0; x < row.length; x++) {
-
-            let cell = row[x];
-            if (cell !== 0) {
-
-                if (cell === gameColumns[y][x + 1] && cell === gameColumns[y][x + 2] && cell === gameColumns[y][x + 3]) {
-
-                    alert(currentPlayer + ' wins')
-                }
-            }
-
-
-
-        }
-    }
-
-
-}
-
-const winHorizontal = function() {
-
-    for (let y = 0; y < gameColumns.length; y++) {
-        let row = gameColumns[y];
-
-        for (let x = 0; x < row.length; x++) {
-            let cell = gameColumns[y][x];
-
-            if (cell !== 0 && y < gameColumns.length - 3) {
-                if (cell === gameColumns[y + 1][x] && cell === gameColumns[y + 2][x] && cell === gameColumns[y + 3][x]) {
-                    alert(currentPlayer + ' wins')
-                }
-            }
-        }
-    }
-}
-
-const winDiagonalRight = function() {
-    for (let y = 0; y < gameColumns.length - 3; y++) {
-        let row = gameColumns[y];
-        for (let x = 0; x < row.length; x++) {
-            cell = gameColumns[y][x];
-            if (cell !== 0) {
-                if (cell === gameColumns[y + 1][x - 1] && cell === gameColumns[y + 2][x - 2] && cell === gameColumns[y + 3][x - 3]) {
-                    alert(currentPlayer + ' wins')
-                }
-            }
-        }
-    }
-}
-
-
-const winDiagonalLeft = function() {
-    for (let y = 0; y < gameColumns.length - 3; y++) {
-        let row = gameColumns[y];
-        for (let x = 0; x < row.length; x++) {
-            cell = gameColumns[y][x];
-            if (cell !== 0) {
-                if (cell === gameColumns[y + 1][x + 1] && cell === gameColumns[y + 2][x + 2] && cell === gameColumns[y + 3][x + 3]) {
-                    alert(currentPlayer + ' wins')
-                }
-            }
-        }
-    }
-}
-
-
 
 var columns = document.querySelectorAll(".column");
 
@@ -162,10 +84,6 @@ for (let i = 0; i < columns.length; i++) {
 
     let column = columns[i];
     column.addEventListener('click', playChip);
-    column.addEventListener('click', winVertical);
-    column.addEventListener('click', winHorizontal);
-    column.addEventListener('click', winDiagonalRight);
-    column.addEventListener('click', winDiagonalLeft);
 
 }
 
